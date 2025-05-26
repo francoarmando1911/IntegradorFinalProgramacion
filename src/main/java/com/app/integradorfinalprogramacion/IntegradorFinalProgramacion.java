0b85ef2c472b56bed4e16a7153073f79cb840c0f
Franco Armando <francoarmando1911@gmail.com>
2025-05-26 19:57:06 -0300

    update

--- src/main/java/com/app/integradorfinalprogramacion/IntegradorFinalProgramacion.java
+++ src/main/java/com/app/integradorfinalprogramacion/IntegradorFinalProgramacion.java
@@ -1,16 +1,16 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.app.integradorfinalprogramacion;
 
-/**
- *
- * @author Franco
- */
+import igu.FirstPage;
+
 public class IntegradorFinalProgramacion {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        FirstPage fp = new FirstPage();
+        fp.setVisible(true);
+        fp.setLocationRelativeTo(null);
+        
     }
 }
+
+
+
