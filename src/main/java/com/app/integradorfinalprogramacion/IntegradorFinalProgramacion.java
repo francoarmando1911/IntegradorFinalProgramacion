555b1a882c4f6bf681fbd4b4edec019f5cb154c7
Franco Armando <francoarmando1911@gmail.com>
2025-05-26 08:12:57 -0300

    Primer commit

---
+++ src/main/java/com/app/integradorfinalprogramacion/IntegradorFinalProgramacion.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.app.integradorfinalprogramacion;
+
+/**
+ *
+ * @author Franco
+ */
+public class IntegradorFinalProgramacion {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
